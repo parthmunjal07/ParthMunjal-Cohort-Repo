b97b46850ae6eba6d04ffa1b697aa092f944cac1
parthmunjal07 <parth.munjal07@gmail.com>
2026-03-25 00:29:55 +0530

    fix: improve formatting and structure of chessboard HTML

--- JS/DOM mini Projects/kanbanBoard/script.js
+++ JS/DOM mini Projects/kanbanBoard/script.js
@@ -1,34 +1,51 @@
-const columns = document.querySelectorAll('.column')
+const columns = document.querySelectorAll(".column");
+let draggedTask = null;
 
-document.addEventListener('click', (e) => {
-    if (e.target.classList.contains('add-btn')){
-        const text = prompt('Enter Your task')
+document.addEventListener("click" , (e)=>{
+    if(e.target.classList.contains("add-btn")){
+        const text = prompt("Enter Task!");
 
-        if (!text) return
+        if(!text) return;
 
-        const task = document.createElement('div')
-        task.className = 'task'
-        task.textContent = text
+        const task = document.createElement("div");
+        task.className = "task";
+        task.textContent = text;
 
-        task.setAttribute('draggable', true)
+        task.setAttribute("draggable" , true);
 
-        e.target.previousElementSibling.appendChild(task)
-    }
-})
-document.addEventListener('dragstart', (e) => {
-    if (e.target.classList.contains('task')){
-        draggedTask = e.target
-        e.target.classList.addEventListener('dragging')
+        e.target.previousElementSibling.appendChild(task);
     }
 })
 
-document.addEventListener('dragend', (e) => {
-    if (e.target.classList.contains('task')){
-        e.target.classList.remove('dragging')
-        draggedTask = null
+document.addEventListener("dragstart" , (e)=>{
+    if(e.target.classList.contains("task")){
+        draggedTask = e.target;
+        e.target.classList.add("dragging")
     }
 })
 
-columns.forEach((col) => {
-    
+document.addEventListener("dragend" , (e)=>{
+    if(e.target.classList.contains("task")){
+        e.target.classList.remove("dragging")
+        draggedTask = null;
+    }
+})
+
+columns.forEach((col)=>{
+    col.addEventListener("dragover" , (e)=>{
+        e.preventDefault()
+        col.classList.add("drag-over")
+    })
+
+      col.addEventListener("dragleave" , (e)=>{
+        col.classList.remove("drag-over")
+    })
+
+      col.addEventListener("drop" , (e)=>{
+        col.classList.remove("drag-over");
+
+        if(draggedTask){
+            col.querySelector(".tasks").appendChild(draggedTask)
+        }
+    })
 })
