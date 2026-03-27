15f8f5b705d636b296499ed17a257def0fa4253f
parthmunjal07 <parth.munjal07@gmail.com>
2026-03-27 18:50:33 +0530

    22 March

---
+++ JS/personal-practice/2-chess/pieceLogic.js
@@ -0,0 +1,36 @@
+function movePawn(pawn) {
+    
+}
+function capturePawn(pawn, target) {
+
+}
+function moveRook(rook) {
+
+}
+function captureRook(rook, target) {
+
+}
+function moveKnight(knight) {
+
+}
+function captureKnight(knight, target) {
+
+}
+function moveBishop(bishop) {
+    
+}
+function captureBishop(bishop, target) {
+
+}
+function moveQueen(queen) {
+
+}   
+function captureQueen(queen, target) {
+
+}
+function moveKing(king) {
+
+}
+function captureKing(king, target) {
+
+}
